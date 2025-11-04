18c87fffa6fae637d6c88c5b5c2db7d014c193a7
JiaxuanLu <165438588+JiaxuanLu@users.noreply.github.com>
2025-11-04 23:18:15 +0100

    Create staging SQL for Northwind invoices

---
+++ models/northwind/staging/stg_invoices.sql
@@ -0,0 +1,11 @@
+{{ config(database='Result_Northwind', schema='stg_northwind') }}
+
+
+with source as (
+
+    select * from {{ source('dl_northwind', 'invoices') }}
+)
+select 
+    *,
+    current_timestamp() as ingestion_timestamp
+from source
