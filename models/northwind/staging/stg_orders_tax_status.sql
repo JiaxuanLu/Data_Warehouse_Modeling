f4bd0ee100f571bf67a343523bf009ba99c29803
JiaxuanLu <165438588+JiaxuanLu@users.noreply.github.com>
2025-11-04 23:20:39 +0100

    Create staging SQL for orders tax status

---
+++ models/northwind/staging/stg_orders_tax_status.sql
@@ -0,0 +1,11 @@
+{{ config(database='Result_Northwind', schema='stg_northwind') }}
+
+
+with source as (
+
+    select * from {{ source('dl_northwind', 'orders_tax_status') }}
+)
+select 
+    *,
+    current_timestamp() as ingestion_timestamp
+from source
